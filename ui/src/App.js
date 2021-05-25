a8eb76b182ac6d79c53695aa4be85df889318640
TheSynEater <37447950+TheSynEater@users.noreply.github.com>
2021-05-25 15:08:46 -0500

    Added environment variables for api and some other changes
    Added environment variables for Mapbox APIs.
    
    Re-created the App.js page with a import to Mapbox.js.
    
    Deleted two files used during testing that are no longer in use.
    
    Co-Authored-By: Ben Cook <4348863+jbencook@users.noreply.github.com>

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,83 +1,15 @@
-//import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import { useState, useRef, useEffect } from 'react';
 
-import './App.css';
-import mapboxgl from 'mapbox-gl';
-//import fetchFakeData from './fetchFakeData';
-//import Popup from "./popup";
+import Mapbox from './Mapbox';
 
-mapboxgl.accessToken = 'test';
 
 const App = () => {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-96.7);
-  const [lat, setLat] = useState(40.8);
-  const [zoom, setZoom] = useState(8);
-  const [pitch, setPitch] = useState(55);
-  const [bearing, setBearing] = useState(0);
-   
-   // this is where all of our map logic is going to live
-  // adding the empty dependency array ensures that the map
-  // is only rendered once
-  useEffect(() => {
-    // create the map and configure it
-    // check out the API reference for more options
-    // https://docs.mapbox.com/mapbox-gl-js/api/map/
-    const map = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/outdoors-v11",
-      center: [lng, lat],
-      zoom: zoom,
-      pitch: pitch,
-      bearing: bearing,
-    });
 
-    //make sure the map is loaded into the DOM
-    map.on("load", () => {
-      // add mapbox terrain dem source for 3d terrain rendering
-      map.addSource("mapbox-dem", {
-        type: "raster-dem",
-        url: "mapbox://mapbox.mapbox-terrain-dem-v1",
-        tileSize: 512,
-        maxZoom: 16,
-      });
-      map.setTerrain({ source: "mapbox-dem" });
-
-      // add the sky layer
-      map.addLayer({
-        id: "sky",
-        type: "sky",
-        paint: {
-          "sky-type": "atmosphere",
-          "sky-atmosphere-sun": [0.0, 90.0],
-          "sky-atmosphere-sun-intensity": 33,
-        },
-      });
-    });
-
-    //on the fly changing the "sidebar values" lat, lng, zoom, pitch, and bearing
-    if (!map) return; // wait for map to initialize
-      map.on('move', () => {
-      setLng(map.getCenter().lng.toFixed(4));
-      setLat(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
-      setPitch(map.getPitch().toFixed(2));
-      setBearing(map.getBearing().toFixed(2));
-    });
-
-    // cleanup function to remove map on unmount
-    return () => map.remove();
-  }, []);
-
-  return (
+  return(
     <div>
-      <div ref={mapContainer} style={{ width: "100%", height: "100vh" }} />
-       <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} | Pitch: {pitch} | Bearing: {bearing}
-       </div>
-       <div ref={mapContainer} className="map-container" />
-     </div>
+      <Mapbox/>
+    </div>
   );
 };
 
