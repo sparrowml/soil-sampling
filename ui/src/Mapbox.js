f3f2af64679815b953ff1b2336c8d63b42a4c187
TheSynEater <37447950+TheSynEater@users.noreply.github.com>
2021-06-08 12:47:24 -0500

    Changed a couple of mapbox elements
    Changed the style of the mapbox and some other minor changes. Also, messed around with the CSS before transitioning into using material-ui.
    
    I will continue to build out the form this week.

--- ui/src/Mapbox.js
+++ ui/src/Mapbox.js
@@ -10,7 +10,7 @@ const Mapbox = () => {
   const [lng, setLng] = useState(-96.7);
   const [lat, setLat] = useState(40.8);
   const [zoom, setZoom] = useState(8);
-  const [pitch, setPitch] = useState(55);
+  const [pitch, setPitch] = useState(0);
   const [bearing, setBearing] = useState(0);
    
    // this is where all of our map logic is going to live
@@ -22,7 +22,7 @@ const Mapbox = () => {
     // https://docs.mapbox.com/mapbox-gl-js/api/map/
     const map = new mapboxgl.Map({
       container: mapContainer.current,
-      style: "mapbox://styles/mapbox/outdoors-v11",
+      style: "mapbox://styles/mapbox/satellite-streets-v11",
       center: [lng, lat],
       zoom: zoom,
       pitch: pitch,
@@ -59,8 +59,8 @@ const Mapbox = () => {
       setLng(map.getCenter().lng.toFixed(4));
       setLat(map.getCenter().lat.toFixed(4));
       setZoom(map.getZoom().toFixed(2));
-      setPitch(map.getPitch().toFixed(2));
-      setBearing(map.getBearing().toFixed(2));
+      //setPitch(map.getPitch().toFixed(2));
+      //setBearing(map.getBearing().toFixed(2));
     });
 
     // cleanup function to remove map on unmount
@@ -69,8 +69,13 @@ const Mapbox = () => {
 
   return (
     <div>
-      <div ref={mapContainer} className="mapbox-container" />
-    </div>
+      <div ref={mapContainer} className="mapbox-container">
+        <div ref={mapContainer} style={{ width: "100%", height: "100" }} />
+          <div className="sidebar">
+          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} 
+       </div>
+      </div>
+     </div>
   );
 };
 
