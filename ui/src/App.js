66a934511a2c5fca013d024a1245fc5599798bcd
TheSynEater <37447950+TheSynEater@users.noreply.github.com>
2021-06-14 16:44:03 -0500

    Form testing (#3)
    * Added mapbox UX and some example layers.
    
    * Added environment variables for api and some other changes
    
    Added environment variables for Mapbox APIs.
    
    Re-created the App.js page with a import to Mapbox.js.
    
    Deleted two files used during testing that are no longer in use.
    
    Co-Authored-By: Ben Cook <4348863+jbencook@users.noreply.github.com>
    
    * Initial Testing of Form in React js
    
    Created CustomForm.js
    
    Created FormInput.js
    
    Changed App.js temporarliy for form testing
    
    *BEN* - Having trouble getting forms to cooperate. Will have to connect soon to get this sorted.
    
    * Continued working on form integration, added the base for the header and footer
    
    Added components directory with base footer and header elements.
    
    Conitued to work on form integration with the mapbox ui.
    
    Did a CSS overhaul and condensed the map down to a fixed size. As well as a few other css changes.
    
    *We may have to switch over to react-routing - which is fine but we can talk more about this on wednesday*
    
    * use material-ui for grid and theme
    
    * update package lock and spacing
    
    * Changed a couple of mapbox elements
    
    Changed the style of the mapbox and some other minor changes. Also, messed around with the CSS before transitioning into using material-ui.
    
    I will continue to build out the form this week.
    
    Co-authored-by: Ben Cook <4348863+jbencook@users.noreply.github.com>
    Co-authored-by: Ben Cook <jbenjamincook@gmail.com>

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,16 +1,42 @@
 import ReactDOM from 'react-dom';
-import { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, Component } from 'react';
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Mapbox from './Mapbox';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+
+import "./App.css";
+
+import Form from "./CustomForm";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 
-const App = () => {
+class App extends Component {
+  render() {
+    return (
+      <main>
+      <CssBaseline />
+      <Container fixed>
+          <div>
+          
+          <div className="Headerbox">
+              <Header/>
+          </div>
 
-  return(
-    <div>
-      <Mapbox/>
-    </div>
-  );
-};
+          <section>
+            <Form/>
+          </section>
+
+          {/* <div className="Mapbox">
+              <Mapbox/>
+          </div> */}
+          
+          </div>
+        </Container>
+      </main>
+    );
+  }
+}
 
 export default App;
--- ui/src/Mapbox.js
+++ ui/src/Mapbox.js
@@ -7,11 +7,10 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN_PK;
 
 const Mapbox = () => {
   const mapContainer = useRef(null);
-  const map = useRef(null);
   const [lng, setLng] = useState(-96.7);
   const [lat, setLat] = useState(40.8);
   const [zoom, setZoom] = useState(8);
-  const [pitch, setPitch] = useState(55);
+  const [pitch, setPitch] = useState(0);
   const [bearing, setBearing] = useState(0);
    
    // this is where all of our map logic is going to live
@@ -23,11 +22,12 @@ const Mapbox = () => {
     // https://docs.mapbox.com/mapbox-gl-js/api/map/
     const map = new mapboxgl.Map({
       container: mapContainer.current,
-      style: "mapbox://styles/mapbox/outdoors-v11",
+      style: "mapbox://styles/mapbox/satellite-streets-v11",
       center: [lng, lat],
       zoom: zoom,
       pitch: pitch,
       bearing: bearing,
+      attributionControl: false,
     });
 
     //make sure the map is loaded into the DOM
@@ -59,8 +59,8 @@ const Mapbox = () => {
       setLng(map.getCenter().lng.toFixed(4));
       setLat(map.getCenter().lat.toFixed(4));
       setZoom(map.getZoom().toFixed(2));
-      setPitch(map.getPitch().toFixed(2));
-      setBearing(map.getBearing().toFixed(2));
+      //setPitch(map.getPitch().toFixed(2));
+      //setBearing(map.getBearing().toFixed(2));
     });
 
     // cleanup function to remove map on unmount
@@ -69,11 +69,12 @@ const Mapbox = () => {
 
   return (
     <div>
-      <div ref={mapContainer} style={{ width: "100%", height: "100vh" }} />
-       <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} | Pitch: {pitch} | Bearing: {bearing}
+      <div ref={mapContainer} className="mapbox-container">
+        <div ref={mapContainer} style={{ width: "100%", height: "100" }} />
+          <div className="sidebar">
+          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} 
        </div>
-       <div ref={mapContainer} className="map-container" />
+      </div>
      </div>
   );
 };
