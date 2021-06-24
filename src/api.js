a8fa6e6ad20b6bee6b14902848b65ceb0aa43649
Ben Cook <jbenjamincook@gmail.com>
2021-06-24 14:03:35 -0500

    api working with cors support

--- src/api.js
+++ src/api.js
@@ -1,10 +1,35 @@
-export async function ssurgoService(values, headers) {
-  const url = 'https://ag-analytics.azure-api.net/ssurgo-v2';
-  return window.fetch(url, {
-    method: 'POST',
-    headers: headers,
-    body: new URLSearchParams(values),
-  })
-    .then(response => response.json())
+// export async function ssurgoService(values, headers) {
+//   const url = 'https://ag-analytics.azure-api.net/ssurgo-v2';
+//   return window.fetch(url, {
+//     method: 'POST',
+//     headers: headers,
+//     body: new URLSearchParams(values),
+//   })
+//     .then(response => response.json())
+//     .catch(console.error);
+// }
+
+export async function polarisService(minlat, maxlat, minlon, maxlon, vari, layer) {
+  const url = `http://152.3.67.2:8000/polaris/${minlon}$${minlat}$${maxlon}$${maxlat}$${layer}$${vari}`;
+  console.log(url)
+  return window.fetch(url)
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
     .catch(console.error);
 }
+
+// import urllib3
+// import numpy as np
+// import json
+// http = urllib3.PoolManager()
+
+// def get_data(minlat,maxlat,minlon,maxlon,var,layer):
+//  #Extract box
+//  string = '%.6f$%.6f$%.6f$%6f$%s$%s' % (minlon, minlat, maxlon, maxlat, layer, var)
+//  r = http.request('GET', 'http://152.3.67.2:8000/polaris/%s' % string)
+//  data = json.loads(r.data)
+//  for var in data:
+//   data[var] = np.array(data[var])
+//  return data
--- src/api.test.js
+++ src/api.test.js
@@ -1,15 +1,26 @@
-import { ssurgoService } from './api';
+import { ssurgoService, polarisService } from './api';
 
-test('basic api call', () => {
-  // Parameters to call SSURGO API
-  const values = {'AOI': '{"geometryType": "esriGeometryPolygon", "features": [{"geometry": {"rings": [[[-85.179, 42.74], [-85.17858886748223, 42.74188232450973], [-85.17858886748223, 42.742675781062474], [-85.1782836915391, 42.742675781062474], [-85.1782226563505, 42.74230956993074], [-85.17529296909521, 42.74230956993074], [-85.17529296909521, 42.74353027370324], [-85.17529296909521, 42.74371337926908], [-85.17492675796348, 42.74389648393566], [-85.17437744126585, 42.744079589501496], [-85.17340087914721, 42.744079589501496], [-85.17327880876996, 42.74749755826576], [-85.17401123013411, 42.74749755826576], [-85.17401123013411, 42.74847412128372], [-85.17529296909521, 42.74847412128372], [-85.17590332008211, 42.74829101571788], [-85.17749023408697, 42.74792480458609], [-85.17761230446422, 42.7470703128447], [-85.1782836915391, 42.746704101712965], [-85.18072509728535, 42.746704101712965], [-85.179, 42.74]]], "spatialReference": {"wkid": 4326}}}]}', 
-  'Soil_Parameter': 'nccpi2all',
-  'Projection': 'EPSG:4326', 
-  'Resolution': 0.00001,
-  'Product':'GeoJSON'};
+// test('basic api call', () => {
+//   // Parameters to call SSURGO API
+//   const values = {'AOI': '{"geometryType": "esriGeometryPolygon", "features": [{"geometry": {"rings": [[[-85.179, 42.74], [-85.17858886748223, 42.74188232450973], [-85.17858886748223, 42.742675781062474], [-85.1782836915391, 42.742675781062474], [-85.1782226563505, 42.74230956993074], [-85.17529296909521, 42.74230956993074], [-85.17529296909521, 42.74353027370324], [-85.17529296909521, 42.74371337926908], [-85.17492675796348, 42.74389648393566], [-85.17437744126585, 42.744079589501496], [-85.17340087914721, 42.744079589501496], [-85.17327880876996, 42.74749755826576], [-85.17401123013411, 42.74749755826576], [-85.17401123013411, 42.74847412128372], [-85.17529296909521, 42.74847412128372], [-85.17590332008211, 42.74829101571788], [-85.17749023408697, 42.74792480458609], [-85.17761230446422, 42.7470703128447], [-85.1782836915391, 42.746704101712965], [-85.18072509728535, 42.746704101712965], [-85.179, 42.74]]], "spatialReference": {"wkid": 4326}}}]}', 
+//   'Soil_Parameter': 'nccpi2all',
+//   'Projection': 'EPSG:4326', 
+//   'Resolution': 0.00001,
+//   'Product':'GeoJSON'};
 
-  // Basic Header Pattern.
-  // Header for using a subscription key.
-  const headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Ocp-Apim-Subscription-Key': process.env.SSURGO_API_KEY}
-  return ssurgoService(values, headers).then(console.log);
-});
+//   // Basic Header Pattern.
+//   // Header for using a subscription key.
+//   const headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Ocp-Apim-Subscription-Key': process.env.SSURGO_API_KEY}
+//   return ssurgoService(values, headers).then(console.log);
+// }, 10000);
+
+test('polaris API test', () => {
+  const minlon = -90.202
+  const minlat = 43.201
+  const maxlon = -90.201
+  const maxlat = 43.202
+  const vari = 'sand'
+  const layer = '0_5'
+
+  return polarisService(minlat,maxlat,minlon,maxlon,vari,layer).then(console.log);
+}, 10000);
