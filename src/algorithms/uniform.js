de823b4af3494cd556d57e5b48deebf1af38a27f
Ben Cook <4348863+jbencook@users.noreply.github.com>
2021-07-28 16:25:45 -0500

    add polygon centroid

---
+++ src/algorithms/uniform.js
@@ -0,0 +1,22 @@
+export function get_polygon_centroid(pts) {
+  var first = pts[0],
+    last = pts[pts.length - 1];
+  if (first[0] !== last[0] || first[1] !== last[1]) pts.push(first);
+  var twicearea = 0,
+    x = 0,
+    y = 0,
+    nPts = pts.length,
+    p1,
+    p2,
+    f;
+  for (var i = 0, j = nPts - 1; i < nPts; j = i++) {
+    p1 = pts[i];
+    p2 = pts[j];
+    f = p1[0] * p2[1] - p2[0] * p1[1];
+    twicearea += f;
+    x += (p1[0] + p2[0]) * f;
+    y += (p1[1] + p2[1]) * f;
+  }
+  f = twicearea * 3;
+  return [x / f, y / f];
+}
