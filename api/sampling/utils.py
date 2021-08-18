d26e7a48a9f85678d0b9ffc58a95a6cffa6e26ff
Ben Cook <4348863+jbencook@users.noreply.github.com>
2021-08-18 14:49:46 -0500

    limit area to <= 2 mi**2

--- api/sampling/utils.py
+++ api/sampling/utils.py
@@ -1,6 +1,12 @@
 import numpy as np
+from shapely.geometry import Polygon
 
 
 def get_utm_string(point: np.ndarray) -> str:
     zone = int((np.floor((point[0] + 180) / 6) % 60) + 1)
     return f"+proj=utm +zone={zone} +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+
+
+def check_area(polygon: np.ndarray) -> None:
+    if Polygon(polygon).area > 10359900:
+        raise ValueError("Polygon too large")
